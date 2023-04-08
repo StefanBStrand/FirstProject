0aae106c8da76ac4039a4f414b0d054d59f9cc39
Stefan Strand <stefanbstrand@outlook.com>
2023-04-08 20:58:39 +0200

    completed log in welcome message

---
+++ userName.js
@@ -0,0 +1,16 @@
+function userName() {
+    
+
+    //Funksjonen skal ta user input og ved onclick button "Enter" endre tekst "Welcome," i h1 tag. 
+
+    //Nr.1: funksjonen skal ta user input
+    
+    let userNameInput=document.getElementById("username").value;
+    console.log(userNameInput);
+
+
+
+
+    //Nr.2: funksjonen skal endre tekst i h1 tag "innerHTML på h1 tag skal endres"
+    document.getElementById("headerTitle").innerHTML = "Welcome, " + userNameInput;
+}
