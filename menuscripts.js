82c61bdb5ee207c087e84c2f2e618df73a993bf0
Stefan Strand <stefanbstrand@outlook.com>
2023-05-21 08:32:58 +0200

    added prettier extension to clean up code

--- menuscripts.js
+++ menuscripts.js
@@ -1,30 +1,25 @@
 window.onload = addActiveClass();
 
-
-
 function addActiveClass() {
-    console.log(document.getElementsByClassName("menuItemLink"));
+  console.log(document.getElementsByClassName("menuItemLink"));
 
-    let current = location.pathname.split('/')[6];
-    console.log(current);
-    if(current === "") return; 
-    let menuItems = document.querySelectorAll('.menuItemLink');
-    for (let i=0, len = menuItems.length; i < len; i++) {
-        if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
-            menuItems[i].className += " menuItemLinkActive";
-        }
+  let current = location.pathname.split("/")[6];
+  console.log(current);
+  if (current === "") return;
+  let menuItems = document.querySelectorAll(".menuItemLink");
+  for (let i = 0, len = menuItems.length; i < len; i++) {
+    if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+      menuItems[i].className += " menuItemLinkActive";
     }
+  }
 }
 
-    
-    
-    console.log(location.pathname);
-    
-    
-    //Remember to console.log everything you dont understand - maybe you'll get it then
+console.log(location.pathname);
 
-    //Funksjonen skal hente alle a elementer i menyen
-    //Funksjonen skal iterere gjennom disse
-    //For hvert element skal den gjøre følgende:
-    //Sjekk om texten på meny-elementet inneholder riktig path
-    //if ja: legg til class "active" på elementet
+//Remember to console.log everything you dont understand - maybe you'll get it then
+
+//Funksjonen skal hente alle a elementer i menyen
+//Funksjonen skal iterere gjennom disse
+//For hvert element skal den gjøre følgende:
+//Sjekk om texten på meny-elementet inneholder riktig path
+//if ja: legg til class "active" på elementet
--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
 function userName() {
-    
+  //Funksjonen skal ta user input og ved onclick button "Enter" endre tekst "Welcome," i h1 tag.
 
-    //Funksjonen skal ta user input og ved onclick button "Enter" endre tekst "Welcome," i h1 tag. 
+  //Nr.1: funksjonen skal ta user input
 
-    //Nr.1: funksjonen skal ta user input
-    
-    let userNameInput=document.getElementById("username").value;
-    console.log(userNameInput);
+  let userNameInput = document.getElementById("username").value;
+  console.log(userNameInput);
 
-
-
-
-    //Nr.2: funksjonen skal endre tekst i h1 tag "innerHTML på h1 tag skal endres"
-    document.getElementById("headerTitle").innerHTML = "Welcome, " + userNameInput;
+  //Nr.2: funksjonen skal endre tekst i h1 tag "innerHTML på h1 tag skal endres"
+  document.getElementById("headerTitle").innerHTML =
+    "Welcome, " + userNameInput;
 }
-
